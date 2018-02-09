8413e23dbb73a7428436055abdd97f14b9c10390
Franc <franciklavz@gmail.com>
2018-02-09 18:02:39 +0100

    Main properties and connections established -Got rid of System.Reflection.InvocationException with a try-catch in Main Window Initialize component method -Preperties properly working and books can be saved to the DB

--- MyBooksApp/MyBooksApp/App.xaml.cs
+++ MyBooksApp/MyBooksApp/App.xaml.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-
+using Realms;
 using Xamarin.Forms;
 
 namespace MyBooksApp
@@ -12,6 +12,7 @@ namespace MyBooksApp
         public App()
         {
             InitializeComponent();
+            
 
             MainPage = new MyBooksApp.MainPage();
         }
--- MyBooksApp/MyBooksApp/ViewModels/MainViewModel.cs
+++ MyBooksApp/MyBooksApp/ViewModels/MainViewModel.cs
@@ -5,17 +5,23 @@ using System.Text;
 using System.Threading.Tasks;
 using System.ComponentModel;
 using Realms;
+using MyBooksApp.Models;
+using Xamarin.Forms;
+
 namespace MyBooksApp.ViewModels
 {
     class MainViewModel : INotifyPropertyChanged
     {
         #region Fields
         private Realm _realmObject;
+        private Rating _selectedRating;
+        private List<Rating> _ratings;
         private String _bookName;
+
         #endregion
 
         #region Properties
-        
+
         public String BookName
         {
             get { return _bookName; }
@@ -25,7 +31,27 @@ namespace MyBooksApp.ViewModels
             }
         }
 
+        public List<Rating> Ratings
+        {
+            get { return _ratings; }
+            set {
+                _ratings = value;
+                PropertyChanged(this, new PropertyChangedEventArgs("Ratings"));
+            }
+        }
 
+        public Rating SelectedRating
+        {
+            get { return _selectedRating; }
+            set {
+                _selectedRating = value;
+                PropertyChanged(this, new PropertyChangedEventArgs("SelectedRating"));
+            }
+        }
+
+        public Command AddBookCommand {
+            get { return new Command(AddBook); }
+        }
 
         #endregion
 
@@ -34,12 +60,33 @@ namespace MyBooksApp.ViewModels
         public MainViewModel()
         {
             _realmObject = Realm.GetInstance();
+            _ratings = _realmObject.All<Rating>().ToList();
+
+            if (_ratings.Count == 0) {
+                _realmObject.Write(() =>
+                {
+                    _realmObject.Add<Rating>(new Rating { Number = 1, Description = "Bad book" });
+                    _realmObject.Add<Rating>(new Rating { Number = 2, Description = "Good book" });
+                    _realmObject.Add<Rating>(new Rating { Number = 3, Description = "Great book" });
+                });
+                _ratings = _realmObject.All<Rating>().ToList();
+            }
+
         }
 
         #endregion
 
         #region Methods
         public event PropertyChangedEventHandler PropertyChanged;
+
+        private void AddBook()
+        {
+            _realmObject.Write(() =>
+            {
+                _realmObject.Add<Book>(new Book() { DisplayName = _bookName, BookRating = _selectedRating });
+            });
+            
+        }
 
         #endregion
 
